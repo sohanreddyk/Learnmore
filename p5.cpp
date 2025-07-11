3735f17348bd7b39ce89b00c332344650154bb72
sohanreddyk <sohan_reddy@outlook.com>
2025-07-11 12:41:29 +0530

    Add initial relocation configuration for aarch64 architecture in p5.yml

---
+++ p5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, n3;
+    cout << "Enter a number:" << endl;
+    cin >> n1;
+    cout << "Enter another number:" << endl;
+    cin >> n2;
+    cout<<"Sum of the numbers is:"<< n1+n2<<endl;
+    cout<<"The difference of the numbers is"<<n2-n1<<endl;
+    cout<<"The product of the numbers is "<<n1*n2<<endl;
+    cout<<"The quotient of the numbers is "<<n2/n1<<endl;
+    cout<<"The remainder of the numbers is "<<n2%n1<<endl;
+    return 0;
+}
