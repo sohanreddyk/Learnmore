bddc7b37b234ff5d6f4b030dd0b029cbce7f6f18
sohanreddyk <sohan_reddy@outlook.com>
2025-07-14 13:34:17 +0530

    case-break

---
+++ day2/p12.cpp
@@ -0,0 +1,20 @@
+//break-statements
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number:" << endl;
+    cin >> n;
+    cout << "The first " << n << " natural numbers are:" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 6) 
+        {
+            break; 
+        }
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ day2/p13.cpp
@@ -0,0 +1,36 @@
+//case-statement
+#include <iostream>
+using namespace std;
+int main() {
+    int day;
+    cout << "Enter a number (1-7) for the day of the week:" << endl;
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid input! Please enter a number between 1 and 7." << endl;
+    }
+
+    return 0;
+}
