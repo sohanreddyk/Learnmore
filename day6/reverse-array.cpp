4337aba79c0d11868d122c49891379aeb13e7a3f
sohanreddyk <sohan_reddy@outlook.com>
2025-07-17 12:59:56 +0530

    reverse-array

---
+++ day6/reverse-array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void reverseArray(int arr[], int size) {
+    int start=0;
+    int end=size-1;
+    while(start < end) {
+        // Swap elements at start and end
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    reverseArray(arr, n);
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
